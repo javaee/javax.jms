5882484ec24033fb7a16d07b539387ecd817f296
Jill Sato <jill.sato@oracle.com>
2015-10-13 23:46:51 +0000

    Update copyright.
    svn path=/trunk/api/javaee-api/javax.jms/; revision=64214

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 #
-# Copyright (c) 2010-2015 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2012-2015 Oracle and/or its affiliates. All rights reserved.
 #
 # The contents of this file are subject to the terms of either the GNU
 # General Public License Version 2 only ("GPL") or the Common Development
@@ -37,6 +37,8 @@
 # and therefore, elected the GPL Version 2 license, then the option applies
 # only if the new code is made subject to such option by the copyright
 # holder.
+#
+
 #
 #------------------------------------------------------   
 #--	BE SURE TO HAVE THE FOLLOWING IN YOUR SETTINGS.XML
